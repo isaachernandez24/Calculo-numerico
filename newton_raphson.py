bb4b8d723c7f7d31e6ed99c1b898349d69ee6d6a
IAHchess2038 <149349216+isaachernandez24@users.noreply.github.com>
2024-02-26 17:02:57 -0400

    Update newton_raphson.py

--- newton_raphson.py
+++ newton_raphson.py
@@ -3,14 +3,19 @@ import math
 def newton_raphson(x0, tol, fx, dfx):
     iteracion = 1
     while True:
+        #se recibe la derivada del archivo tests.py 
         derivada = dfx(x0)
+
+        #si la derivada es 0 el sistema se detiene 
         if derivada == 0:
             print("La derivada es cero. El método no puede continuar.")
             return None
-        
+
+        #calculo del error 
         x1 = x0 - fx(x0) / derivada
         error = abs(x1 - x0)
-        
+
+        # solo se detiene si el error es menor a la tolerancia que se obtiene en el test
         if error < tol:
             return x1, error
         
