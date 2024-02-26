aea5077fdcf58198ec1062954415be2f2eec9e03
IAHchess2038 <149349216+isaachernandez24@users.noreply.github.com>
2024-02-26 16:49:38 -0400

    Update biseccion.py

--- biseccion.py
+++ biseccion.py
@@ -1,8 +1,9 @@
 import math
 
+#funcion para realizar el metdo de biseccion 
 def bisection_method(func, a, b, tol=0.01, max_iter=100):
     iter_count = 0
-    
+    #el bucle seguira iterando hasta que el numero de iteraciones sea igual a 100 o que el error sea menor a la tolerancia que tiene 
     while (b - a) / 2 > tol and iter_count < max_iter:
         m = (a + b) / 2
         f_a = func(a)
@@ -13,7 +14,7 @@ def bisection_method(func, a, b, tol=0.01, max_iter=100):
             b = m
         else:
             a = m
-        
+        #calculo del error
         error = (b - a) / 2
         iter_count += 1
 
